20de968772d0e50eaae7843aea955b4e644487da
ReadWriteV <ReadWriteV@outlook.com>
2022-03-14 18:28:31 +0800

    use Win32 API

--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
-#include <QtWidgets/QApplication>
-#include "widget.h"
-
 #include <Windows.h>
 
-HHOOK keyHook;
-Widget *main_widget = nullptr;
-
-LRESULT CALLBACK keyProc(int nCode, WPARAM wParam, LPARAM lParam)
+INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, INT nCmdShow)
 {
-    KBDLLHOOKSTRUCT *pkbhs = (KBDLLHOOKSTRUCT *)lParam;
-
-    if (pkbhs->vkCode == VK_ESCAPE)
-    {
-        main_widget->pause_key_down();
-    }
-    return CallNextHookEx(keyHook, nCode, wParam, lParam);
-}
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    main_widget = &w;
-    keyHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyProc, nullptr, 0);
-    return a.exec();
+    MessageBox(NULL, L"hello world", L"Test", MB_OK);
+    return 0;
 }
